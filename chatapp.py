c0ec3d1303c6fd520091be0861122f88633ab8ea
Yalan Meng <mengyalan@gmail.com>
2013-04-22 02:54:45 -0500

    Init with Email handler and MainPage handler implemented

---
+++ chatapp.py
@@ -0,0 +1,46 @@
+import jinja2
+import os
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        username = 'placeholder'
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(username=username))
+
+        
+
+class SendChatLog(webapp2.RequestHandler):
+    def post(self):
+        log = self.request.get('log')
+        librarian = "UIUC Librarian <michaelkanglu@gmail.com>"
+        title = "Your Chat History with UIUC Library"
+        patron = "Patron <mengyalan@gmail.com>"
+        # email = self.request.get('email')
+        # nickname = self.request.get('nickname')
+        # patron = nickname + ' <' + email + '>'
+        message = mail.EmailMessage(sender=librarian, subject=title)
+        
+        message.to = patron
+        message.body = log
+        
+        message.send()
+        self.redirect('/')
+        
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/email', SendChatLog)
+], debug=True)
