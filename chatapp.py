48219c928bf3693261a52ee75ef6f90c78bb2db5
Michael Lu <mklu2@illinois.edu>
2013-05-06 03:02:05 -0500

    Removed references to jinja

--- chatapp.py
+++ chatapp.py
@@ -1,4 +1,3 @@
-import jinja2
 import os
 import webapp2
 
@@ -7,15 +6,6 @@ from google.appengine.api import mail
 from google.appengine.ext.webapp import template
 
 import models
-
-
-# We set a parent key on the 'Greetings' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
-
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'])
 
 
 class MainPage(webapp2.RequestHandler):
